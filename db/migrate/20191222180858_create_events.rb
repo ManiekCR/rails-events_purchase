7dca7391365d40b32b5c7bcbfe49f35e46cc45a6
Marian Caron <caron.marian@gmail.com>
2019-12-22 19:10:13 +0100

    first commit

---
+++ db/migrate/20191222180858_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.date :date
+      t.time :time
+      t.text :description
+      t.integer :availability
+
+      t.timestamps
+    end
+  end
+end
