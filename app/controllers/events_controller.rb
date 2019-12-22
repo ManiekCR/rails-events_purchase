4ee9d2098c9a27505947ae5f4409fd18fa05d188
Marian Caron <caron.marian@gmail.com>
2019-12-22 22:26:54 +0100

    events controller created

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,17 @@
+class EventsController < ApplicationController
+  before_action :set_event, only: :show
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+end
