d86877c2c903548fd23423ee5913294024e74f23
Marian Caron <caron.marian@gmail.com>
2020-01-07 20:34:09 +0100

    updated stripe event initializer

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,13 @@
 Rails.configuration.stripe = {
   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-  secret_key:      ENV['STRIPE_SECRET_KEY']
+  secret_key:      ENV['STRIPE_SECRET_KEY'],
+  signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
   # don't forget to add these values in the .env file
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
+StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
+
+StripeEvent.configure do |events|
+  events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
+end
