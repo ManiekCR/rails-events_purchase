520da8b745a61c94b99d86b0f89d2155404614b1
Marian Caron <caron.marian@gmail.com>
2020-01-07 15:19:31 +0100

    added orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,21 @@
+class OrdersController < ApplicationController
+  def create
+    event = Event.find(params[:event_id])
+    order = Order.create!(event: event, name: event.name, amount: event.price, state: 'pending', user: current_user)
+
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [{
+        name: event.name,
+        amount: event.price_cents,
+        currency: 'eur',
+        quantity: 1
+      }],
+      success_url: order_url(order),
+      cancel_url: order_url(order)
+    )
+
+    order.update(checkout_session_id: session.id)
+    redirect_to new_order_payment_path(order)
+  end
+end
