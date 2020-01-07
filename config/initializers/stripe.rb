3bdab31af07c392c12a1e34113aa2bacb43339ba
Marian Caron <caron.marian@gmail.com>
2020-01-07 15:04:20 +0100

    comment added

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,7 @@
 Rails.configuration.stripe = {
   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
   secret_key:      ENV['STRIPE_SECRET_KEY']
+  # don't forget to add these values in the .env file
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
